e594b34fd10679c6b1393e47a16f76531d1c4ee8
linkzepp <adriftchen@gmail.com>
2020-12-10 01:27:35 +0800

    完成更新圖片功能,撰寫api程式可以共用更多狀況

--- base.php
+++ base.php
@@ -25,6 +25,13 @@ $addstr=[
     'menu'=>"新增選單",
 ];
 
+$uploadimg=[
+    'image'=>['更新校園映像圖片','校園映像圖片'],
+    'mvim'=>['更換動畫圖片','動畫圖片'],
+    'title'=>['更新網站標題圖片','網站標題圖片'],
+
+];
+
 class DB{
 
     protected $table;
