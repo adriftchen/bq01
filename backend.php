dc01b3003772abb02c1c260886f94b4d0d546b6d
linkzepp <adriftchen@gmail.com>
2020-12-11 10:21:58 +0800

    網站標題管理圖片顯示錯誤修正

--- backend.php
+++ backend.php
@@ -23,7 +23,7 @@
     
     <div id="main">
         <a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
-            <div class="ti" style="background:url(&#39;img/<?=$Title->find(['sh'=>1])['img'];?>/&#39;); background-size:cover;"></div> <!-- 有顯示的那張圖'sh'=1 -->
+            <div class="ti" style="background:url(&#39;img/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div> <!-- 有顯示的那張圖'sh'=1 -->
             <!--標題-->
         </a>
         <div id="ms">
