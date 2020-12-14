9714ba2b4e4c4bc96d1440268c5118e06ab81929
linkzepp <adriftchen@gmail.com>
2020-12-14 14:06:29 +0800

    完成加強功能/登入登出功能

--- backend.php
+++ backend.php
@@ -1,5 +1,13 @@
 <!-- 記得include_once base -->
-<?php include_once "base.php";?>
+<?php include_once "base.php"
+
+// if(empty($_SESSION['login'])){
+//     echo "非法入侵";
+//     exit();
+// }
+
+
+;?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=title -->
@@ -82,7 +90,7 @@
                             <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
                                 class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
                             </td>
-                            <td><button onclick="lo(&#39;index.php?do=login?&#39;)"
+                            <td><button onclick="lo(&#39;api/logout.php?&#39;)"
                                     style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
                         </tr>
                     </tbody>
